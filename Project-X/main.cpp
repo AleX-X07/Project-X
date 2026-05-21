f296a4cd35c7dfe8f4631d0d436d000bddb24f9f
Alex <asoulier@gaming.tech>
2026-05-21 15:15:06 +0200

    push sfml

--- Project-X/main.cpp
+++ Project-X/main.cpp
@@ -1,5 +1,26 @@
-﻿#include <iostream>
+﻿#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
 }
