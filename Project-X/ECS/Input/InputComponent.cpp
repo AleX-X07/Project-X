ba98439df810d2c3c771ad117fbe138f75105010
Alex <asoulier@gaming.tech>
2026-06-01 12:22:12 +0200

    push xml

--- Project-X/ECS/Input/InputComponent.cpp
+++ Project-X/ECS/Input/InputComponent.cpp
@@ -9,7 +9,7 @@ InputComponent::InputComponent(Object* _owner)
 void InputComponent::update(float deltaTime)
 {
     auto movementsComp = owner->getComponent<movementsComponent>();
-    if (sf::Keyboard::isKeyPressed(Input::getInput()->getKey("Left")) || (sf::Joystick::getAxisPosition(0, sf::Joystick::Axis::X) < -deadzone)))
+    if (sf::Keyboard::isKeyPressed(Input::getInput()->getKey("Left")) || (sf::Joystick::getAxisPosition(0, sf::Joystick::Axis::X) < -deadzone))
     {
         if (movementsComp != nullptr)
         {
