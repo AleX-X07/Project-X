67029a955a5ddeea66118001e4046a25a97eb327
Xernss <steverpicx@gmail.com>
2026-07-21 14:09:51 +0200

    .

--- Project-X/Pattern/Factories.cpp
+++ Project-X/Pattern/Factories.cpp
@@ -22,7 +22,7 @@ std::unordered_map <
         return new AiMobSpawner_Desert(obj, currentScene->getVecObjects());
     }},
     {"BossManager",    [](Object* obj, const nlohmann::json& ecs, Scene* currentScene) -> Component* {
-        return new BossManager(obj, ecs["args"][0], currentScene->getVecObjects(), false);
+        return new BossManager(obj, ecs["args"][0], currentScene->getVecObjects(), ecs["args"][1]);
     }},
     // Box
     {"HurtBox", [](Object* obj, const nlohmann::json& ecs, Scene* currentScene) -> Component* {
